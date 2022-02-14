2e21b8d99502f12ffb35f0d2348caa46ba0a2212
unhappyJavaMan <lemel1993@gmail.com>
2022-02-14 14:54:30 +0800

    change name

---
+++ src/main/java/com/roomTypePic/model/RoomTypePicVO.java
@@ -0,0 +1,32 @@
+package com.roomTypePic.model;
+
+public class RoomTypePicVO {
+	private Integer room_photo_id; 
+	private Integer room_type_id;
+	private byte[] room_pic;
+	
+	public Integer getRoom_photo_id() {
+		return room_photo_id;
+	}
+	public void setRoom_photo_id(Integer room_photo_id) {
+		this.room_photo_id = room_photo_id;
+	}
+	public Integer getRoom_type_id() {
+		return room_type_id;
+	}
+	public void setRoom_type_id(Integer room_type_id) {
+		this.room_type_id = room_type_id;
+	}
+	public byte[] getRoom_pic() {
+		return room_pic;
+	}
+	public void setRoom_pic(byte[] room_pic) {
+		this.room_pic = room_pic;
+	}
+
+	
+
+	
+	
+
+}
