b8bbc85d3998b8c3dd99dfd805d816dca207d1a9
unhappyJavaMan <lemel1993@gmail.com>
2022-02-14 14:07:03 +0800

    addd aaaaa

--- src/main/java/com/roomtype/controller/RoomTypeServlet.java
+++ src/main/java/com/roomtype/controller/RoomTypeServlet.java
@@ -26,7 +26,7 @@ public class RoomTypeServlet extends HttpServlet {
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
 		
-		if ("getOne_For_Update".equals(action)) { // 來自listAllEmp.jsp的請求
+		if ("getOne_For_Update".equals(action)) { // 來自listAllEmp.jsp的請求aaaaaaaaa
 
 			List<String> errorMsgs = new LinkedList<String>();
 			// Store this set in the request scope, in case we need to
